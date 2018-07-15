b133bff7997211b405fae253cd018113767510da
unknown <haydnjmorris@googlemail.com>
2018-07-15 11:11:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {
+  NODE_ENV,
+  PORT,
+} = process.env;
+
+const app = express();
+
+app.use(bodyParser);
+
+require('./routes')(app);
+
+if (NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT || 4000);
