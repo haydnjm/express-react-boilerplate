bd1b22f5eea3d654d2c811f582cc041a71378c57
unknown <haydnjmorris@googlemail.com>
2018-07-15 12:41:08 +0100

    react refactor

--- client/src/containers/Root.js
+++ client/src/containers/Root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class App extends Component {
+class Root extends Component {
 
   state = {
     status: 'Loading',
@@ -23,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Root;
